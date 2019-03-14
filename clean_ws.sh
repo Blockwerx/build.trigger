0b4e47587a23333929eb3b8ecfdf3fe8fe0f8963
Lizo <lizo@blockwerx.com>
2019-03-14 09:44:59 +0200

    add bash script for docker commands

--- clean_ws.sh
+++ clean_ws.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #clear exited containers
+docker ps -f status=exited -aq
 `docker rm  $(docker ps -f status=exited -aq)` || true;
 
 #clear dangling images
+docker images -f dangling=true -aq
 `docker rmi  $(docker images -f dangling=true -aq)` || true;
 
 #clear voulme

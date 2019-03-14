38b4e87761f770e668b9a55220c8928ffc01d091
Lizo <lizo@blockwerx.com>
2019-03-14 09:40:19 +0200

    add bash script for docker commands

--- clean_ws.sh
+++ clean_ws.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #clear exited containers
-`docker rm  $(docker ps -f status=exited -aq) || true`;
+`docker rm  $(docker ps -f status=exited -aq)` || true;
 
 #clear dangling images
-`docker rmi  $(docker images -f dangling=true -aq) || true`;
+`docker rmi  $(docker images -f dangling=true -aq)` || true;
 
 #clear voulme
-`docker volume prune -f || true`;
+docker volume prune -f;
 
 # clear workspace
-`ls /var/lib/jenkins/workspace/`;
-`rm -rf /var/lib/jenkins/workspace/`;
+ls /var/lib/jenkins/workspace/;
+rm -rf /var/lib/jenkins/workspace/;
